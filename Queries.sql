010e7944d09a862fb05e073f7c7dce2c1bb6df3b
Alexander Medvedev <medvedevae@pgkweb.ru>
2021-08-07 00:51:31 +0300

    Changes in queries

--- Queries.sql
+++ Queries.sql
@@ -1,24 +1,24 @@
-create table if not exists performer (
-	performer_id integer primary key,
-	performer_name varchar(100),
-	genre_id integer references genre(genre_id)
-);
-
-create table if not exists album (
-	album_id integer primary key,
-	album_name varchar(1000),
-	year integer,
-	performer_name varchar(100) references performer(performer_id)
-);
-
-create table if not exists track (
-	track_id integer primary key,
-	track_name varchar(100),
-	duration integer,
-	album_name varchar(1000) references album(album_id)
-);
-
-create table if not exists genre (
+create table if not exists test.genre (
 	genre_id integer primary key,
 	genre_name varchar(100)
 	);
+
+create table if not exists test.performer (
+	performer_id integer primary key,
+	performer_name varchar(100),
+	genre_id integer not null references test.genre(genre_id)
+);
+
+create table if not exists test.album (
+	album_id integer primary key,
+	album_name varchar(1000),
+	year integer,
+	performer_id integer not null references test.performer(performer_id)
+);
+
+create table if not exists test.track (
+	track_id integer primary key,
+	track_name varchar(100),
+	duration integer,
+	album_id integer not null references test.album(album_id)
+);
